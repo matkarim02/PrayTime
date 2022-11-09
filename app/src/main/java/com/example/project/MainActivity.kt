449809eee06e5fc8184e7b5589b3d2db4ca23ac5
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-11-09 16:34:42 +0600

    создали слушатель нажатии BottomNavigationView

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -6,9 +6,19 @@ import com.example.project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bNav.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.vremya ->{}
+                R.id.qibla ->{}
+                R.id.namaz ->{}
+                R.id.dua ->{}
+            }
+            true
+        }
     }
 }
