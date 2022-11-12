a32633055ff9fe0583de5b893ae6b2f49cfa97ac
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-11-13 04:38:22 +0600

    связали фрагмент из активити в мейн

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,21 +2,23 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.bNav.selectedItemId = R.id.vremya
-        binding.bNav.setOnNavigationItemSelectedListener{
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        binding.bNav.setOnItemSelectedListener() {
             when(it.itemId){
-                R.id.vremya ->{
-
-                }
+                R.id.vremya ->{}
                 R.id.qibla ->{}
                 R.id.namaz ->{}
                 R.id.dua ->{}
