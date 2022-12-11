7c32d83d38d9507b6bf1969b9ca67f6abb76701f
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-12-12 05:53:23 +0600

    добавили model с помощью плагина json to kotlin

---
+++ app/src/main/java/com/example/project/model/TodayWeather.kt
@@ -0,0 +1,11 @@
+package com.example.project.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class TodayWeather(
+    @SerializedName("pressure")
+    val pressure: String,
+    @SerializedName("temperature")
+    val temperature: String
+)
