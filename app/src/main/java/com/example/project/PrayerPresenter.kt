e7027aad6a9059086a7fd7a02d9eab061c125c51
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-12-12 06:12:01 +0600

    create presenter to build fun get data from api

---
+++ app/src/main/java/com/example/project/PrayerPresenter.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import android.content.Context
+
+class PrayerPresenter(context: Context){
+    val prayerView = context as PrayerView
+
+    fun getDataFromApi() {
+
+    }
+}
