870ebfb9c6d1748f36b37f60ad5206b79f22c706
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-12-12 06:06:43 +0600

    create fun for  retrive data  from api

--- app/src/main/java/com/example/project/PrayerView.kt
+++ app/src/main/java/com/example/project/PrayerView.kt
@@ -1,5 +1,9 @@
 package com.example.project
 
+import com.example.project.model.Item
+
 interface PrayerView {
+    fun onDataCompleteFromApi(prayer:Item)
+    fun onDataErrorFromApi(throwable: Throwable)
 
 }
