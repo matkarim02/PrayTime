1b1f968a7ac5e8758ee746ed1f0bff27e60336d7
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-12-12 07:00:37 +0600

    add endpoint fromurl

--- app/src/main/java/com/example/project/PrayerPresenter.kt
+++ app/src/main/java/com/example/project/PrayerPresenter.kt
@@ -1,11 +1,28 @@
 package com.example.project
 
 import android.content.Context
+import com.example.project.model.Item
+import com.example.project.model.prayer
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PrayerPresenter(context: Context){
     val prayerView = context as PrayerView
 
-    fun getDataFromApi() {
+    fun getDataFromApi(city:String) {
+        RetrofitService.create()
+            .getApi(city)
+            .enqueue(object : Callback<prayer> {
+                override fun onFailure(call: Call<prayer>, t: Throwable) {
+                    prayerView.onDataErrorFromApi(t)
+                }
+
+                override fun onResponse(call: Call<prayer>, response: Response<prayer>) {
+                    prayerView.onDataCompleteFromApi(response.body()?.items?.get(0) as Item )
+                }
+
+            })
 
     }
 }
--- app/src/main/java/com/example/project/RetrofitService.kt
+++ app/src/main/java/com/example/project/RetrofitService.kt
@@ -1,6 +1,26 @@
 package com.example.project
 
+import com.example.project.model.prayer
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
 interface RetrofitService {
 
+    @GET("{city}.json?key=ba1bf1eec09e02ac9bff09142c4d2371")
+    fun getApi(@Path("city") city: String) : Call<prayer>
 
+    companion object {
+        fun create(): RetrofitService{
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://muslimsalat.com")
+                .build()
+            return retrofit.create(RetrofitService::class.java)
+        }
+
+    }
 }
