d1be40b2f1ad6abf7ef5e610ab612d50724d1d77
Vakhabov Matkarim <matkarimvakhabov@gmail.com>
2022-12-13 17:20:15 +0600

    add RecyclerView to DuaFragment

--- app/src/main/java/com/example/project/screens/dua/DuaFragment.kt
+++ app/src/main/java/com/example/project/screens/dua/DuaFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project.CustomAdapter
 import com.example.project.R
+import com.example.project.Zikr
 
 
 class DuaFragment : Fragment() {
-
+private lateinit var recyclerView: RecyclerView
+private lateinit var zikrList: ArrayList<Zikr>
+private lateinit var customAdapter: CustomAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_dua, container, false)
+        recyclerView = view.findViewById<RecyclerView>(R.id.recycler)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        zikrList = ArrayList()
+
+        zikrList.add(Zikr("Zikr", "ASDFSAFASFSA"))
+        zikrList.add(Zikr("Zikr", "ASDFSAFASFSA"))
+
+
+        customAdapter = CustomAdapter(zikrList)
+        recyclerView.adapter = customAdapter
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dua, container, false)
+        return view
     }
 
 }
